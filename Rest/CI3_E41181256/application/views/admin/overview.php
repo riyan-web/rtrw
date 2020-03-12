c045fa53c25645b1704bc6a06bb430d59488dea8
Rizki Widya Pratama <55204773+mrrest017@users.noreply.github.com>
2020-03-12 08:47:01 +0700

    Update overview.php

--- Rest/CI3_E41181256/application/views/admin/overview.php
+++ Rest/CI3_E41181256/application/views/admin/overview.php
@@ -88,7 +88,7 @@
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                        <?php echo $this->session->userdata("nama"); ?>
                     </div>
                 </nav>
             </div>
